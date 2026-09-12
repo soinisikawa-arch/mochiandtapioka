91a76ab1ffd0d843275bf06d80963b6160ecea2a
soinisikawa-arch <soi.nisikawa@gmail.com>
2026-09-12 10:07:31 +0900

    Load refreshed menu styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist } from 'next/font/google';
 import './globals.css';
+import './menu-refresh.css';
 
 const geist = Geist({ variable: '--font-geist', subsets: ['latin'] });
 
